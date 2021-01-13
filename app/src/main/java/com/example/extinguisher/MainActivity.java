df3e0197cf53e1bc6ae072a6a7425ae08cd9b3e3
AngelaZhang123 <angelazhang3913@gmail.com>
2021-01-13 12:47:51 -0800

    App skeleton finished

--- app/src/main/java/com/example/extinguisher/MainActivity.java
+++ app/src/main/java/com/example/extinguisher/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.extinguisher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMenuButton = (Button) findViewById(R.id.menu_button);
+        mMenuButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/extinguisher/MenuActivity.java
+++ app/src/main/java/com/example/extinguisher/MenuActivity.java
@@ -2,13 +2,54 @@ package com.example.extinguisher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button mListButton, mInfoButton, mProfileButton, mLevelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        mListButton = (Button) findViewById(R.id.checklist_button);
+        mListButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, CheckListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mInfoButton = (Button) findViewById(R.id.info_button);
+        mInfoButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mProfileButton = (Button) findViewById(R.id.profile_button);
+        mProfileButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mLevelButton = (Button) findViewById(R.id.level_button);
+        mLevelButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, LevelActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
