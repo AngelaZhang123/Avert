0262e7dacd1126b326c78ac053de6d7de4b85d8a
AngelaZhang123 <angelazhang3913@gmail.com>
2021-01-22 13:28:15 -0800

    Checklist done- checkboxes not rigged yet

--- app/src/main/java/com/example/extinguisher/CheckListActivity.java
+++ app/src/main/java/com/example/extinguisher/CheckListActivity.java
@@ -3,12 +3,16 @@ package com.example.extinguisher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class CheckListActivity extends AppCompatActivity {
+
+    private CheckBox item1, item2, item3, item4, item5, item6, item7, item8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_list);
+
     }
 }
