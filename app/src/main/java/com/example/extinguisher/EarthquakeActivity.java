c73d283c5a0a34ab7973c2518254f7a27abf6571
AlexaBaxter <alexabaxter01@gmail.com>
2021-01-23 21:19:42 -0800

    EQ Level 1

--- app/src/main/java/com/example/extinguisher/EarthquakeActivity.java
+++ app/src/main/java/com/example/extinguisher/EarthquakeActivity.java
@@ -2,13 +2,25 @@ package com.example.extinguisher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
 public class EarthquakeActivity extends AppCompatActivity {
-
+    private Button L1Button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake);
+
+        L1Button = (Button) findViewById(R.id.level1Button);
+        L1Button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EarthquakeActivity.this, EQLevel1Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
